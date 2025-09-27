6af8052f5ebb54971373b270e00d9de241247b24
Henry Sabio <henry@sabio.sh>
2025-09-27 17:32:50 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const app = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to the Crypto Dashboard</h1>
+    </div>
+    );
+}
+ 
+export default app;
